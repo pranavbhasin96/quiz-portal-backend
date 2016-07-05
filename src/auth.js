f9b0c71659905957d84c700d3ef8c8b93b52db21
Anubhab Sen <anubhabsen@gmail.com>
2016-07-06 00:14:10 +0530

    Send name and email on successful auth along with token
    * This can then be used in the frontend
    * Issue #6
    * PR #9

--- src/auth.js
+++ src/auth.js
@@ -4,12 +4,12 @@ var config = require('./config');
 var models = require('./models');
 var middleware = require('./middleware');
 
-function sendToken (user_id, res) {
-  jwt.sign({}, config.secret, { subject: user_id.toString(), expiresIn: "2h" }, function (err, token) {
+function sendToken (user, res) {
+  jwt.sign({}, config.secret, { subject: user.id.toString(), expiresIn: "2h" }, function (err, token) {
     if (err != null) {
       throw err;
     } else {
-      res.send({token: token});
+      res.send({token: token, email: user.email, name: user.name});
     }
   });
 }
@@ -24,7 +24,7 @@ router.post ('/login', middleware.isNotAuthenticated, (req, res, next) => {
   models.User.find({where : {
     email, password
   }}).then(user => {
-    if (user) sendToken(user.id, res);
+    if (user) sendToken(user, res);
     else res.sendStatus(401);
   }).catch(next)
 })
@@ -34,13 +34,13 @@ router.post ('/register',  middleware.isNotAuthenticated, (req, res, next) => {
 
   return models.User.find({where : { email }}).then(user => {
     if (user) {
-      if (user.password == password) sendToken(user.id, res);
+      if (user.password == password) sendToken(user, res);
       else res.sendStatus(401);
     } else {
       return models.User.create({
         email, password, name
       }).then(user => {
-        sendToken(user.id, res);
+        sendToken(user, res);
       })
     }
   }).catch(next)
