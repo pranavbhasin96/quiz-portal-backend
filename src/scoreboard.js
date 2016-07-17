ac74185cb52b068462198e87f149e44df2ecbcf3
pranav bhasin <pranav.bhasin@students.iiit.ac.in>
2016-07-18 01:52:58 +0530

    Added Score Table
    >Issue #3

---
+++ src/scoreboard.js
@@ -0,0 +1,12 @@
+var models = require('./models');
+
+module.exports = function (req, res, next){
+  models.User.findAll({
+    attributes: ['name', 'score'],
+    order: [ [ 'score', 'DESC' ] ]
+  }).then(scores => {
+    if (scores) res.send(scores);
+    else res.sendStatus(400);
+    next();
+  })
+};
